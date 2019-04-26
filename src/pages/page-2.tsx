4f37b0dd763174637445985dccec829eefbde518
Andrew Bryson <andrew@codebro.io>
2019-04-26 18:50:44 +0200

    GE Added glsl

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,5 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { Shaders, Node, GLSL } from "gl-react";
+import { Surface } from "gl-react-dom";
+
+const shaders = Shaders.create({
+  helloBlue: {
+    frag: GLSL`
+precision highp float;
+varying vec2 uv;
+uniform float blue;
+void main() {
+  gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+}`
+  }
+});
+
+function HelloBlue() {
+  return <Node shader={shaders.helloBlue} uniforms={{ blue: 0.5 }} />;
+}
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,7 +26,9 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Surface width={300} height={300}>
+      <HelloBlue/>
+    </Surface>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
